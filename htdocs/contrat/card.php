b390b340b555185c1600df9aa8e7459451ec9698
stefkpl <github@avtek.fr>
2026-09-25 15:48:56 +0200

    Add intervention button if module is enabled
    Signed-off-by: stefkpl <github@avtek.fr>

--- htdocs/contrat/card.php
+++ htdocs/contrat/card.php
@@ -2400,6 +2400,16 @@ if ($action == 'create') {
 						'enabled' => true,
 					);
 				}
+				if (isModEnabled('intervention') && $object->status > 0) {
+					$langs->load("interventions");
+					$arrayofcreatebutton[] = array(
+						'url' => '/fichinter/card.php?action=create&origin='.$object->element.'&originid='.$object->id.'&socid='.$object->thirdparty->id,
+						'label' => $langs->trans('AddIntervention'),
+						'lang' => 'interventions',
+						'perm' => $user->hasRight('ficheinter', 'creer') ? true : false,
+						'enabled' => true,
+					);
+				}				
 				if (count($arrayofcreatebutton)) {
 					unset($params['attr']['title']);
 					print dolGetButtonAction('', $langs->trans("Create"), 'default', $arrayofcreatebutton, '', true, $params);
